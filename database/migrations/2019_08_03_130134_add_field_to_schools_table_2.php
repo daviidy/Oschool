142eda1b29514a91e267d9416a6128ed26e1e9df
aouman <37551703+aouman@users.noreply.github.com>
2019-08-21 00:55:32 +0000

    master cloner

--- database/migrations/2019_08_03_130134_add_field_to_schools_table_2.php
+++ database/migrations/2019_08_03_130134_add_field_to_schools_table_2.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class DropFieldToSchoolsTable extends Migration
+class AddFieldToSchoolsTable2 extends Migration
 {
     /**
      * Run the migrations.
@@ -14,7 +14,7 @@ class DropFieldToSchoolsTable extends Migration
     public function up()
     {
         Schema::table('schools', function (Blueprint $table) {
-            $table->dropColumn('description');
+            $table->longText('description');
         });
     }
 
