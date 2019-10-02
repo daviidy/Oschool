c8d3ed4d5f33ec4187db63fa89f00c51fc4caf00
aouman <37551703+aouman@users.noreply.github.com>
2019-10-02 08:53:16 +0000

    responsive à refaire

--- public/js/admin_views/curriculum.js
+++ public/js/admin_views/curriculum.js
@@ -12,3 +12,30 @@ $(".tch-btn-header-icon-2").click(function() {
   $(".tch-btn-header-icon-2").css("display","none");
   $(".tch-btn-header-icon").css("display","block");
 });
+
+// AVANTAGES OSCHOOL tab
+//AVANTAGES tab desktop
+$('.tabs a').on('click', function (event) {
+    event.preventDefault();
+    $('.active').removeClass('active');
+    $(this).parent().addClass('active');
+    $('.tab-panel .ng-star-inserted').hide();
+    $($(this).attr('href')).show();
+});
+
+$('.steps-nav a:first .next-step').trigger('click'); // Default
+
+//Avantage tab MOBILE
+
+$('.tab--bar a').on('click', function (event) {
+    event.preventDefault();
+    var active_tab_selector = $('.tab--bar.active > a').attr('href');
+    var actived_nav = $('.tab--bar a');
+		actived_nav.removeClass('active');
+    $(this).parent().addClass('inactive');
+    $(active_tab_selector).removeClass('inactive');
+		$(active_tab_selector).addClass('inactive');
+    $('.tab-panel').hide();
+    $($(this).attr('href')).show();
+});
+$('.tab--bar a:second').trigger('click'); // Default
