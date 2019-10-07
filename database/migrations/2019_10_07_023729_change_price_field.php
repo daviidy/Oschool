49ebe476ce1a74457c07ba6f86522d3697a091ca
daviidy <yaodavidarmel@gmail.com>
2019-10-07 02:57:25 +0000

    add mobile money payment gateway 6

---
+++ database/migrations/2019_10_07_023729_change_price_field.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChangePriceField extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('pricings', function (Blueprint $table) {
+            $table->dropColumn('price');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('pricings', function (Blueprint $table) {
+            //
+        });
+    }
+}
