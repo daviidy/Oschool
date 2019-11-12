c7fc280bc28c7a51a7dfd20d3f3f6cc53220fd81
aouman <37551703+aouman@users.noreply.github.com>
2019-11-12 08:35:10 +0000

    path

--- public/js/path-slide.js
+++ public/js/path-slide.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   //toggle the component with class accordion_body
-  $(".myg-accordion__title").click(function() {
+  /*$(".myg-accordion__title").click(function() {
     if ($('.myg-accordion__panel').is(':visible')) {
       $(".myg-accordion__panel p").slideUp(300);
     }
@@ -9,23 +9,17 @@ $(document).ready(function() {
     } else {
       $(this).next(".myg-accordion__panel p").slideDown(300);
     }
+  });*/
+
+
+  $(".myg-accordion__header").click(function(){
+      $(".myg-accordion__header").each(function(){
+        $(this).parent().removeClass("active");
+        $(this).removeClass("active");
+      });
+      $(this).parent().addClass("active");
+      $(this).addClass("active");
   });
-});
 
-$(function() {
-  $('.tabs nav a').on('click', function() {
-    show_content($(this).index());
-  });
 
-  show_content(0);
-
-  function show_content(index) {
-    // Make the content visible
-    $('.tabs .content.visible').removeClass('visible');
-    $('.tabs .content:nth-of-type(' + (index + 1) + ')').addClass('visible');
-
-    // Set the tab to selected
-    $('.tabs nav a.selected').removeClass('selected');
-    $('.tabs nav a:nth-of-type(' + (index + 1) + ')').addClass('selected');
-  }
 });
