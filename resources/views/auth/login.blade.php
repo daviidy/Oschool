f8cd73a8d5cff30eb5445c1f37eb9f727dcf8598
Samuel Bouye <bouyesamuel@gmail.com>
2019-11-19 17:03:24 +0000

    add features authentification with facebook and gmail

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,74 +2,99 @@
 
 @section('content')
  
+
+  <style>
+    /*! CSS Used from: https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css */
+a{background-color:transparent;}
+a:active,a:hover{outline:0;}
+b{font-weight:700;}
+@media print{
+*,:after,:before{color:#000!important;text-shadow:none!important;background:0 0!important;-webkit-box-shadow:none!important;box-shadow:none!important;}
+a,a:visited{text-decoration:underline;}
+a[href]:after{content:" (" attr(href) ")";}
+a[href^="#"]:after{content:"";}
+}
+*{-webkit-box-sizing:border-box;-moz-box-sizing:border-box;box-sizing:border-box;}
+:after,:before{-webkit-box-sizing:border-box;-moz-box-sizing:border-box;box-sizing:border-box;}
+a{color:#337ab7;text-decoration:none;}
+a:focus,a:hover{color:#23527c;text-decoration:underline;}
+a:focus{outline:5px auto -webkit-focus-ring-color;outline-offset:-2px;}
+.btn{display:inline-block;padding:6px 12px;margin-bottom:0;font-size:14px;font-weight:400;line-height:1.42857143;text-align:center;white-space:nowrap;vertical-align:middle;-ms-touch-action:manipulation;touch-action:manipulation;cursor:pointer;-webkit-user-select:none;-moz-user-select:none;-ms-user-select:none;user-select:none;background-image:none;border:1px solid transparent;border-radius:4px;}
+.btn:active:focus,.btn:focus{outline:5px auto -webkit-focus-ring-color;outline-offset:-2px;}
+.btn:focus,.btn:hover{color:#333;text-decoration:none;}
+.btn:active{background-image:none;outline:0;-webkit-box-shadow:inset 0 3px 5px rgba(0,0,0,.125);box-shadow:inset 0 3px 5px rgba(0,0,0,.125);}
+.btn-primary{color:#fff;background-color:#337ab7;border-color:#2e6da4;}
+.btn-primary:focus{color:#fff;background-color:#286090;border-color:#122b40;}
+.btn-primary:hover{color:#fff;background-color:#286090;border-color:#204d74;}
+.btn-primary:active{color:#fff;background-color:#286090;border-color:#204d74;}
+.btn-primary:active:focus,.btn-primary:active:hover{color:#fff;background-color:#204d74;border-color:#122b40;}
+.btn-primary:active{background-image:none;}
+.btn-lg{padding:10px 16px;font-size:18px;line-height:1.3333333;border-radius:6px;}
+.btn-block{display:block;width:100%;}
+/*! CSS Used from: https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css */
+.fa{display:inline-block;font:normal normal normal 14px/1 FontAwesome;font-size:inherit;text-rendering:auto;-webkit-font-smoothing:antialiased;-moz-osx-font-smoothing:grayscale;}
+.fa-facebook:before{content:"\f09a";}
+/*! CSS Used from: Embedded */
+.btn{border-radius:50px;outline:none!important;}
+.login-form .btn{font-size:18px;line-height:26px;font-weight:bold;text-align:center;}
+.social-btn .btn{color:#fff;margin:10px 0;font-size:15px;border-radius:50px;text-indent:10px;font-weight:normal;border:none;text-align:center;}
+.social-btn .btn:hover{opacity:0.9;}
+.social-btn .btn-primary{background:#507cc0;}
+.social-btn .btn i{margin-right:9px;font-size:20px;min-width:25px;position:relative;top:2px;}
+.login-form a{color:#5cb85c;}
+/*! CSS Used fontfaces */
+@font-face{font-family:'FontAwesome';src:url('https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/fonts/fontawesome-webfont.eot?v=4.7.0');src:url('https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/fonts/fontawesome-webfont.eot#iefix&v=4.7.0') format('embedded-opentype'),url('https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/fonts/fontawesome-webfont.woff2?v=4.7.0') format('woff2'),url('https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/fonts/fontawesome-webfont.woff?v=4.7.0') format('woff'),url('https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/fonts/fontawesome-webfont.ttf?v=4.7.0') format('truetype'),url('https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/fonts/fontawesome-webfont.svg?v=4.7.0#fontawesomeregular') format('svg');font-weight:normal;font-style:normal;}
+  
+  
+  
+  
+  </style>
+
+
 <style>
 
-  /* Shared */
-  .loginBtn {
-    box-sizing: border-box;
-    position: relative;
-    /* width: 13em;  - apply for fixed size  okk*/
-    margin: 0.3em;
-    padding: 10px 30px 10px 46px;
-    border: none;
-    text-align: left;
-    line-height: 34px;
-    white-space: nowrap;
-    border-radius: 0.2em;
-    font-size: 16px;
-    color: #FFF;
-  }
-  .loginBtn:before {
-    content: "";
-    box-sizing: border-box;
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 34px;
-    height: 100%;
-  }
-  .loginBtn:focus {
-    outline: none;
-  }
-  .loginBtn:active {
-    box-shadow: inset 0 0 0 32px rgba(0,0,0,0.1);
-  }
-  
-  
-  /* Facebook */
-  .loginBtn--facebook {
-    background-color: #4C69BA;
-    background-image: linear-gradient(#4C69BA, #3B55A0);
-    /*font-family: "Helvetica neue", Helvetica Neue, Helvetica, Arial, sans-serif;*/
-    text-shadow: 0 -1px 0 #354C8C;
-  }
-  .loginBtn--facebook:before {
-    border-right: #364e92 1px solid;
-    background: url('https://s3-us-west-2.amazonaws.com/s.cdpn.io/14082/icon_facebook.png') 6px 6px no-repeat;
-  }
-  .loginBtn--facebook:hover,
-  .loginBtn--facebook:focus {
-    background-color: #5B7BD5;
-    background-image: linear-gradient(#5B7BD5, #4864B1);
-  }
-  
-  
-  /* Google */
-  .loginBtn--google {
-    /*font-family: "Roboto", Roboto, arial, sans-serif;*/
-    background: #DD4B39;
-  }
-  .loginBtn--google:before {
-    border-right: #BB3F30 1px solid;
-    background: url('https://s3-us-west-2.amazonaws.com/s.cdpn.io/14082/icon_google.png') 6px 6px no-repeat;
-  }
-  .loginBtn--google:hover,
-  .loginBtn--google:focus {
-    background: #E74B37;
-  }
-  </style>
-  
-
+/*! CSS Used from: https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css */
+a{background-color:transparent;}
+a:active,a:hover{outline:0;}
+b{font-weight:700;}
+@media print{
+*,:after,:before{color:#000!important;text-shadow:none!important;background:0 0!important;-webkit-box-shadow:none!important;box-shadow:none!important;}
+a,a:visited{text-decoration:underline;}
+a[href]:after{content:" (" attr(href) ")";}
+a[href^="#"]:after{content:"";}
+}
+*{-webkit-box-sizing:border-box;-moz-box-sizing:border-box;box-sizing:border-box;}
+:after,:before{-webkit-box-sizing:border-box;-moz-box-sizing:border-box;box-sizing:border-box;}
+a{color:#337ab7;text-decoration:none;}
+a:focus,a:hover{color:#23527c;text-decoration:underline;}
+a:focus{outline:5px auto -webkit-focus-ring-color;outline-offset:-2px;}
+.btn{display:inline-block;padding:6px 12px;margin-bottom:0;font-size:14px;font-weight:400;line-height:1.42857143;text-align:center;white-space:nowrap;vertical-align:middle;-ms-touch-action:manipulation;touch-action:manipulation;cursor:pointer;-webkit-user-select:none;-moz-user-select:none;-ms-user-select:none;user-select:none;background-image:none;border:1px solid transparent;border-radius:4px;}
+.btn:active:focus,.btn:focus{outline:5px auto -webkit-focus-ring-color;outline-offset:-2px;}
+.btn-g:focus,.btn-g:hover{color:#333;text-decoration:none;}
+.btn:active{background-image:none;outline:0;-webkit-box-shadow:inset 0 3px 5px rgba(0,0,0,.125);box-shadow:inset 0 3px 5px rgba(0,0,0,.125);}
+.btn-danger-g{color:#fff;background-color:#d9534f;border-color:#d43f3a;}
+.btn-danger-g:focus{color:#fff;background-color:#c9302c;border-color:#761c19;}
+.btn-danger-g:hover{color:#fff;background-color:#c9302c;border-color:#ac2925;}
+.btn-danger-g:active{color:#fff;background-color:#c9302c;border-color:#ac2925;}
+.btn-danger-g:active:focus,.btn-danger:active:hover{color:#fff;background-color:#ac2925;border-color:#761c19;}
+.btn-danger-g:active{background-image:none;}
+.btn-lg{padding:10px 16px;font-size:18px;line-height:1.3333333;border-radius:6px;}
+.btn-block{display:block;width:100%;}
+.btn-block+.btn-block{margin-top:5px;}
+/*! CSS Used from: https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css */
+.fa{display:inline-block;font:normal normal normal 14px/1 FontAwesome;font-size:inherit;text-rendering:auto;-webkit-font-smoothing:antialiased;-moz-osx-font-smoothing:grayscale;}
+.fa-google:before{content:"\f1a0";}
+/*! CSS Used from: Embedded */
+.btn{border-radius:50px;outline:none!important;}
+.login-form .btn{font-size:18px;line-height:26px;font-weight:bold;text-align:center;}
+.social-btn .btn{color:#fff;margin:10px 0;font-size:15px;border-radius:50px;text-indent:10px;font-weight:normal;border:none;text-align:center;}
+.social-btn .btn:hover{opacity:0.9;}
+.social-btn .btn-danger{background:#df4930;}
+.social-btn .btn i{margin-right:9px;font-size:20px;min-width:25px;position:relative;top:2px;}
+.login-form a{color:#5cb85c;}
+/*! CSS Used fontfaces */
+@font-face{font-family:'FontAwesome';src:url('https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/fonts/fontawesome-webfont.eot?v=4.7.0');src:url('https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/fonts/fontawesome-webfont.eot#iefix&v=4.7.0') format('embedded-opentype'),url('https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/fonts/fontawesome-webfont.woff2?v=4.7.0') format('woff2'),url('https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/fonts/fontawesome-webfont.woff?v=4.7.0') format('woff'),url('https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/fonts/fontawesome-webfont.ttf?v=4.7.0') format('truetype'),url('https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/fonts/fontawesome-webfont.svg?v=4.7.0#fontawesomeregular') format('svg');font-weight:normal;font-style:normal;}
+</style>
   <section class="sign-up-form account-form">
 
     <div class="form-aligner">
@@ -152,19 +177,19 @@
                   
                     <br>
   
-                    <button class="loginBtn loginBtn--facebook">
+                    {{-- <button class="loginBtn loginBtn--facebook">
                       <a class="btn btn-link" href="{{ url('login/facebook') }}">
                       <span class="_8jan">{{ __('Connexion avec facebook') }}</span>
                     </a>
-                    </button>
+                    </button> --}}
   
-                    <button class="loginBtn loginBtn--google">
+                    {{-- <button class="loginBtn loginBtn--google">
                       <a class="btn btn-link" href="{{ url('login/gmail') }}">
                       <span class="_8jan">{{ __('Connexion avec gmail') }}</span>
-                    </a>
-                    </button>
-                  
-                  
+                    </a> --}}
+
+                    <a href="{{ url('login/facebook') }}" class="btn btn-primary btn-lg btn-block"><i class="fa fa-facebook"></i> {{ __('Connexion avec ') }}<b>Facebook</b></a>
+                    <a href="{{ url('login/gmail') }}" class="btn btn-danger-g btn-lg btn-block btn-g"><i class="fa fa-google"></i> {{__('Connexion avec ') }} <b>Google</b></a>
                   </div>
                     
                 <div id="right-box-spinner" class="right-box-spinner create-form-spinner hidden"></div>
