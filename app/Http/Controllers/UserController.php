8db5cc21fab79e5812537157b55906645968ea5c
Samuel Bouye <bouyesamuel@gmail.com>
2019-11-20 14:02:48 +0000

    allow users to change its password

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Hash;
 use Auth;
 use Image;
 
@@ -114,5 +116,63 @@ class UserController extends Controller
     public function destroy(User $user)
     {
         //
+    }
+
+
+
+    public function changePassword()
+    {
+        // if()
+        return view('users.changePassword');
+    }
+
+    public function admin_credential_rules(array $data)
+    {
+    $messages = [
+        'current_password.required' => 'Please enter current password',
+        'password.required' => 'Please enter password',
+    ];
+
+    $validator = Validator::make($data, [
+        'current_password' => 'required',
+        'password' => 'required|same:password',
+        'password_confirmation' => 'required|same:password',     
+    ], $messages);
+
+    return $validator;
+    } 
+
+    public function editPassword(Request $request)
+    {
+            if(Auth::Check())
+        {
+            $request_data = $request->All();
+            $validator = $this->admin_credential_rules($request_data);
+            if($validator->fails())
+            {
+            return response()->json(array('error' => $validator->getMessageBag()->toArray()), 400);
+            }
+            else
+            {  
+            $current_password = Auth::User()->password;           
+            if(Hash::check($request_data['current_password'], $current_password))
+            {           
+                $user_id = Auth::User()->id;                       
+                $obj_user = User::find($user_id);
+                $obj_user->password = Hash::make($request_data['password']);;
+                $obj_user->save(); 
+                return redirect()->back()->with('status', 'La modification de votre mot de passe a effectuées');
+            }
+            else
+            {           
+                $error = array('current_password' => 'Please enter correct current password');
+                return response()->json(array('error' => $error), 400);   
+            }
+            }        
+        }
+        else
+        {
+            return redirect()->to('/');
+        }    
     }
 }
