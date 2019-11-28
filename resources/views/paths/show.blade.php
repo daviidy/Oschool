f603c91b9e21b156edfab8c46d8171b829773001
daviidy <yaodavidarmel@gmail.com>
2019-11-28 18:11:49 +0000

    fix bug issues on courses show page 2

--- resources/views/paths/show.blade.php
+++ resources/views/paths/show.blade.php
@@ -4,11 +4,16 @@
 @section('description', $course->subtitle)
 @section('content')
 
-
+@auth
 @if(Auth::user()->courses->contains($course->id))
 @include('includes.default.course_page.path_enrolled')
 @else
 @include('includes.default.course_page.path_new')
 @endif
+@endauth
+
+@guest
+@include('includes.default.course_page.path_new')
+@endguest
 
 @endsection
