9ce86dd07ae4a7bb37ae5c59795dbf4aea84da00
daviidy <yaodavidarmel@gmail.com>
2019-12-15 13:39:54 +0000

    fix bug for course students list 2

--- resources/views/admin_views/courses/students.blade.php
+++ resources/views/admin_views/courses/students.blade.php
@@ -338,6 +338,7 @@ a:hover,a:focus{color:#167b72;text-decoration:none;}
                            {{number_format((count($user->lessons->where('course_id', $course->id)) / count($course->lessons)) * 100)}}%
                            @else
                            {{number_format((count($user->deliverables->where('course_id', $course->id)->where('status', '1')) / count($course->projects)) * 100)}}
+                           @endif
                        </td>
                         <td>
                             <!---->
