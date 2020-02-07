0c30bbfd72f53eb42beba1c6d7f843351aef8a09
daviidy <yaodavidarmel@gmail.com>
2020-02-07 13:15:27 +0000

    add css in deliverables comment file

--- database/migrations/2020_02_05_013724_create_announcements_table.php
+++ database/migrations/2020_02_05_013724_create_announcements_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class EditFullTextTypeFieldLessonsTable extends Migration
+class CreateAnnouncementsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,9 @@ class EditFullTextTypeFieldLessonsTable extends Migration
      */
     public function up()
     {
-        Schema::table('lessons', function (Blueprint $table) {
-            $table->text('text');
+        Schema::create('announcements', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
         });
     }
 
@@ -25,8 +26,6 @@ class EditFullTextTypeFieldLessonsTable extends Migration
      */
     public function down()
     {
-        Schema::table('lessons', function (Blueprint $table) {
-            $table->dropColumn('full_text');
-        });
+        Schema::dropIfExists('announcements');
     }
 }
