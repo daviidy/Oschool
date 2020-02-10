98c655135008d62a72f107bd4c5d2cd343390576
daviidy <yaodavidarmel@gmail.com>
2020-02-10 16:32:04 +0000

    students list issue 4

--- resources/views/admin_views/courses/students.blade.php
+++ resources/views/admin_views/courses/students.blade.php
@@ -288,7 +288,7 @@ a:hover,a:focus{color:#167b72;text-decoration:none;}
                 <tbody ui-sortable="sortableOptions" ng-model="products" class="ng-pristine ng-untouched ng-valid ui-sortable ng-not-empty">
                     <!---->
                     @foreach($course->users as $user)
-                        @if($user->purchases->where('user_id', $user->id)->where('status', 'Validé')->where('course_id', $course->id)
+                        @if($user->purchases->where('user_id', $user->id)->where('status', 'Validé')->where('course_id', $course->id))
                     <tr what="product" which="Free Course" ng-repeat="product in products" class="border-bottom ui-sortable-handle">
                         <td what="id"><span ng-bind="'#' + product.id">{{$user->name}}</span><span class="space"></span><span class="space"></span>
                             <!---->
