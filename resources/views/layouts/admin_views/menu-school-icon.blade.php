c796427dcaaf304d6505013e83a1513a2cdc367c
aouman <37551703+aouman@users.noreply.github.com>
2020-04-27 18:28:56 +0000

    icone catégorie

--- resources/views/layouts/admin_views/menu-school-icon.blade.php
+++ resources/views/layouts/admin_views/menu-school-icon.blade.php
@@ -485,6 +485,22 @@ a:hover,a:focus{color:#167b72;text-decoration:none;}
 
                         <!---->
                         <!---->
+                        <li what="nav item" ui-sref-active="active" style=""
+                          ng-class="{ 'hide-on-expand': hideOnExpand, 'show-on-expand': showOnExpand, 'pin-bottom-level-': pinToBottom, 'pin-bottom': pinToBottom, 'top-border': topBorder, 'force-active': (buttonActive == true) }"
+                          ng-if="::permissions.can('view_migrated_notifications')" text="Notifications" icon="icon icon-envelope" sref="admin.settings.notifications">
+                            <!----><a what="link" ui-sref="admin.settings.notifications" ng-if="sref &amp;&amp; !migrated" ng-class="{ 'text-only': minimal, 'small-link': small, 'never-highlight': neverHighlight }" href="/schoolAdmin/{{$school->id}}/categorie">
+                                <!----><i ng-if="::icon" ng-class="::icon" tooltip="Emails" tooltip-placement="right" tooltip-trigger="mouseenter" tooltip-append-to-body="true" tooltip-class="primary-nav-tooltip" class="fas fa-box-open"></i>
+                                <!---->
+                                <!---->
+                                <!---->
+                                <!----><span ng-bind="::text" ng-class="textClass" class="menu-item-label">Catégories</span></a>
+                            <!---->
+                            <!---->
+                            <!---->
+                        </li>
+
+                        <!---->
+                        <!---->
                         <!---->
                         <hr ng-if="::permissions.can('view_courses')">
                         <!---->
