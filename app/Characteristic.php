f80e87e06cbc55c9611630294006c82c784efb6e
daviidy <yaodavidarmel@gmail.com>
2020-06-12 21:56:35 +0000

    offers issue

--- app/Characteristic.php
+++ app/Characteristic.php
@@ -10,7 +10,7 @@ class Characteristic extends Model
 
     public function offers()
     {
-        return $this->belongsToMany('App\Offer','characteristic_offer',);
+        return $this->belongsToMany('App\Offer');
     }
 
 }
