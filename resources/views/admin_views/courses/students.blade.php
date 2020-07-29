5210ddd31f6db38c742c1b3a01c25a552754f5c6
aouman <37551703+aouman@users.noreply.github.com>
2020-07-29 11:25:56 +0000

    text color

--- resources/views/admin_views/courses/students.blade.php
+++ resources/views/admin_views/courses/students.blade.php
@@ -850,7 +850,7 @@ a:hover,a:focus{color:#167b72;text-decoration:none;}
                                      {{number_format((count($user->deliverables->where('course_id', $course->id)->where('status', '1')) / count($course->projects)) * 100)}}
                                      @endif
                                     </span></td>
-                                <td ui-sref="admin.users.user.information({ id: user.id })" ng-show="showColumn(title, 'unsubscribe')" class="_2kIOe" href="/admin/users/32429890/information"><span ng-bind="getUnsubscribeFromMarketingEmails(user)"><a href="#myNoteStudent" rel="modal:open"><button  type="button" class="btn" style="background-color: #1bcc1b">Voir les notes</button></a></span>
+                                <td ui-sref="admin.users.user.information({ id: user.id })" ng-show="showColumn(title, 'unsubscribe')" class="_2kIOe" href="/admin/users/32429890/information"><span ng-bind="getUnsubscribeFromMarketingEmails(user)"><a href="#myNoteStudent" rel="modal:open"><button  type="button" class="btn" style="background-color: #1bcc1b; color:#fff">Voir les notes</button></a></span>
                               	</td>
                               <td ui-sref="admin.users.user.information({ id: user.id })" class="_2kIOe" href="/admin/users/32429890/information"><span am-time-ago="user.created_at">
                                 <div ng-if="product.is_published" class="pull-right">
