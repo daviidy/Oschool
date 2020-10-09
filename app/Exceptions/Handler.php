66df3b136a48e0443edef85f1917a40027f5e215
daviidy <yaodavidarmel@gmail.com>
2020-10-09 14:41:36 +0000

    add custom error pages

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -46,9 +46,21 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        /*
         if ($exception instanceof CustomException) {
             return response()->view('errors.404', [], 500);
         }
         return parent::render($request, $exception);
+        */
+        if ($this->isHttpException($exception)) {
+             if ($exception->getStatusCode() == 404) {
+                 return response()->view('errors.' . '404', [], 404);
+             }
+             if ($exception->getStatusCode() == 500) {
+                 return response()->view('errors.' . '500', [], 500);
+             }
+         }
+         return parent::render($request, $exception);
+
     }
 }
