511831497d8fd9896c3bed2a57619323dad436ee
aouman <k2.aouman@gmail.com>
2021-01-10 23:37:09 +0000

    mise en place du bouton inscriver-vous dans le menu de la page d'accueil. et activation du popup de connection dans les boutons.

--- resources/views/includes/auths/register.blade.php
+++ resources/views/includes/auths/register.blade.php
@@ -16,7 +16,7 @@
   </head>
   <body class="">
     <!-- Modal -->
-    <div class="modal fade bg-light" id="registerModal" tabindex="-1" aria-labelledby="registerModalLabel" aria-hidden="true">
+    <div class="modal fade bg-light" id="registerModal" tabindex="-1" aria-labelledby="registerModalLabel" aria-hidden="true" style="z-index:9999;">
       <span class="float-right position-absolute rounded-circle svg-delete close" data-dismiss="modal" aria-label="Close"><svg width="1.5em" height="1.5em" viewBox="0 0 16 16" class="bi bi-x" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
           <path fill-rule="evenodd" d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
         </svg></span>
--- resources/views/layouts/homePageMenu.blade.php
+++ resources/views/layouts/homePageMenu.blade.php
@@ -73,8 +73,25 @@
 
             </ul>
               @guest
-                <div class="ml-auto">
-                  <a href="{{route('login')}}" class="text-decoration-none p-3 bg-oschool text-white rounded-pill text-center">
+                <div class="ml-auto d-flex">
+                  <a href="{{route('login')}}" class="bg-danger text-decoration-none d-md-block d-none p-3 bg-oschool text-white rounded-pill text-center mr-2 mr-md-3" data-dismiss="modal" data-toggle="modal" data-target="#registerModal">
+                    Inscrivez-vous
+                  </a>
+                  <!--mobile-->
+                  <a href="{{route('login')}}" class="bg-danger text-decoration-none d-md-none d-block p-3 bg-oschool text-white rounded-pill text-center mr-2 mr-md-3" data-toggle="modal" data-target="#registerModal">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-box-arrow-in-left" viewBox="0 0 16 16">
+                      <path fill-rule="evenodd" d="M10 3.5a.5.5 0 0 0-.5-.5h-8a.5.5 0 0 0-.5.5v9a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-2a.5.5 0 0 1 1 0v2A1.5 1.5 0 0 1 9.5 14h-8A1.5 1.5 0 0 1 0 12.5v-9A1.5 1.5 0 0 1 1.5 2h8A1.5 1.5 0 0 1 11 3.5v2a.5.5 0 0 1-1 0v-2z"/>
+                      <path fill-rule="evenodd" d="M4.146 8.354a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H14.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3z"/>
+                    </svg>
+                  </a>
+                  <a href="{{route('login')}}" class="text-decoration-none d-md-none d-block p-3 bg-oschool text-white rounded-pill text-center mr-2 mr-md-3" data-toggle="modal" data-target="#loginModal">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-person-check" viewBox="0 0 16 16">
+                      <path d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H1s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C9.516 10.68 8.289 10 6 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>
+                      <path fill-rule="evenodd" d="M15.854 5.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 0 1 .708-.708L12.5 7.793l2.646-2.647a.5.5 0 0 1 .708 0z"/>
+                    </svg>
+                  </a>
+                  <!--End mobile-->
+                  <a href="#" class="text-decoration-none d-md-block d-none p-3 bg-oschool text-white rounded-pill text-center" data-toggle="modal" data-target="#loginModal">
                     Connectez-vous
                   </a>
 
@@ -115,4 +132,6 @@
           </nav>
         </header>
         @yield('content')
+        @include('includes.auths.login')
+        @include('includes.auths.register')
         @include('includes.footer')
